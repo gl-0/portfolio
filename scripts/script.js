3f038a977fb142a1741aba10437e8c462fd4bf8a
gl-0 <langdongt@outlook.com>
2024-09-24 14:30:48 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,44 @@ const body = document.querySelector('body');
 const btn = document.getElementById('btn');
 const logo = document.getElementById('logo');
 const projects = document.getElementsByClassName('projects');
+
+let sun = '\u{263C}';
+let moon = '\u{263E}';
+
+let currentTheme = localStorage.getItem('myTheme');
+let btnText = localStorage.getItem('button');
+
 //? DARK MODE BUTTON - INDEX, ABOUT, CONTACT ME, PROJECTS ///
-toggleDark = () => {
-  
-  changeText = () => {
-    if(btn.innerHTML === "test"){
-        btn.innerHTML = "sun";
-    } else {
-        btn.innerHTML = "test";
-    }
+console.log(`Testing...theme is ${currentTheme}`);
+console.log(btnText);
+
+function changeText(){
+  // if(localStorage.getItem('myTheme') !== 'dark'){
+  //   btn.innerText = moon
+  // } else {
+  //   btn.innerText = sun
+  // }
+  if(localStorage.getItem('myTheme') !== 'dark'){
+    btn.innerHTML = moon;
+    localStorage.setItem('button', btn.innerHTML);
+  } else {
+    btn.innerHTML = sun;
+    localStorage.setItem('button', btn.innerHTML);
+  }
+
+  // btn.innerHTML = localStorage.getItem('button');
+  // if(btn.innerHTML !== btnText){
+  //   btn.innerHTML = btnText;
+  //   localStorage.setItem('button', btn.innerHTML);
+  // }
+
+  // if(btnText === moon){
+  //   btn.innerHTML = sun;
+  //   localStorage.setItem('button', btn.innerHTML)
+  //     } else {
+  //       btn.innerHTML = moon;
+  //   localStorage.setItem('button', btn.innerHTML)
+  //   }
   };
 
   changeLogo = () => {
@@ -21,24 +50,42 @@ toggleDark = () => {
         logo.src = "images/logo.png"
     }
   };
+
+toggleDark = () => {
+  let light = document.getElementsByClassName('light');
+  
+  html.classList.toggle('dark');
+  localStorage.setItem('myTheme', html.className);
+  console.log(`Current...theme is ${localStorage.getItem('myTheme')}`)
+  for (let i = 0; i < light.length; i++){
+    light[i].classList.toggle('dark');
+};
+
+  changeText();
+  changeLogo();
+
 }
 
-btn.addEventListener('click', function () {             
-  let light = document.getElementsByClassName('light'); //* Targets the button to switch to Dark Mode
-  html.classList.toggle('dark');                        //* changes html class to 'dark'
-  localStorage.setItem('myTheme', html.className);      //* sets local storage to match html class('dark')
-                                                //! Important to do this AFTER the class has been updated to dark mode.
-  for (let i = 0; i < light.length; i++){
-      light[i].classList.toggle('dark');
-  };
-});
-toggleDark();
+// btn.addEventListener('click', function () {             
+//   let light = document.getElementsByClassName('light'); //* Targets the button to switch to Dark Mode
+  
+//   html.classList.toggle('dark');                        //* changes html class to 'dark'
+//   localStorage.setItem('myTheme', html.className);      //* sets local storage to match html class('dark')
+//                                                         //! Important to do this AFTER the class has been updated to dark mode.
+//   console.log(`Current...theme is ${localStorage.getItem('myTheme')}`)
+  
+//   for (let i = 0; i < light.length; i++){
+//       light[i].classList.toggle('dark');
+//   };
+//   toggleDark();
+// });
+
 if (localStorage.getItem('myTheme')){          
     html.classList.toggle(localStorage.getItem('myTheme'));
 }
 
 
-
+//* ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
 // localStorage.setItem('theme', 'light');
 // localStorage.getItem('theme');
